0d0afd0c32652a9a2b0ab415a96aba4849c819ec
sunikutty <142837260+sunikutty@users.noreply.github.com>
2023-08-21 23:25:29 -0700

    Update streamlit_app.py
    Corrected spelling of streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("My parents new healthy Diner")
-streamlite.header ("Breakfast Menu")
-streamlite.text ("Omega 3 and Blueberry Oatmeal")
-streamlite.text ("Kale Spinach Rocket Smoothie")
-streamlite.text ("Hard Boiled Free Range Egg")
+streamlit.header ("Breakfast Menu")
+streamlit.text ("Omega 3 and Blueberry Oatmeal")
+streamlit.text ("Kale Spinach Rocket Smoothie")
+streamlit.text ("Hard Boiled Free Range Egg")
