a5c6356071474b008e7521b7b29a17febded0f77
sunikutty <142837260+sunikutty@users.noreply.github.com>
2023-08-25 15:21:30 -0700

    Update streamlit_app.py
    Fruity vice fruite update was added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 streamlit.title('My parents new healthy Diner')
 streamlit.header ('Breakfast Favourites')
 streamlit.text ('🥣 Omega 3 and Blueberry Oatmeal')
@@ -17,3 +19,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
